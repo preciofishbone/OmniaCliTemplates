8ce3e4223db75773ee09e1fa57e1e8e677416729
andy <andy@ekdahls.net>
2018-06-07 09:03:16 +0200

    Added serviceid

--- src/0.1.0/projects/web/omnia.service.ts
+++ src/0.1.0/projects/web/omnia.service.ts
@@ -1,7 +1,7 @@
 ﻿import { Composer, DevelopmentEnvironment } from "@omnia/tooling/composers";
 
 Composer
-    .registerManifest("cfbb6092-29d8-47fa-9866-4f755c04720d", "Omnia Cloud Web Auth")
-    .registerService({ description: "Omnia Platform" })
+    .registerManifest("$serviceid$", "$namespace$")
+    .registerService({ description: "Description of $namespace$" })
     .AsWebApp()
    
