ed4cf26eae2396767286d1f85ba0671623fb5703
andy <andy@ekdahls.net>
2018-07-26 16:03:57 +0200

    Updated vuewebcomponent

--- src/0.1.0/items/vuewebcomponent/vuewebcomponent.tsx
+++ src/0.1.0/items/vuewebcomponent/vuewebcomponent.tsx
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import { Component, Watch, Prop } from 'vue-property-decorator';
 import { vueCustomElement, IWebComponentInstance, WebComponentBootstrapper, Localize, Inject } from "@omnia/fx";
-import { $outputname$Styles } from './$outputname$.css';
+import { I$outputname$, $outputname$Data } from './I$outputname$';
+import * as $outputname$Styles from './$outputname$.css';
 
 @Component
 export default class $outputname$ extends Vue implements IWebComponentInstance, I$outputname$ {
 
     @Prop({ default: false }) required: boolean;
-    @Prop({ default: { title: 'Hello from $outputname$!' } }) data?: MyTestCompData
+    @Prop({ default: { title: 'Hello from $outputname$!' } }) data?: $outputname$Data
 
     mounted() {
 
