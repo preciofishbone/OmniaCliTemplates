c77145c3205a54dcabac4a421163d2e47ebc5472
Duong Tran Lap <lapduongbkit@gmail.com>
2020-06-09 16:05:55 +0700

    Add item templates for localization

---
+++ src/4.0.0/items/localization/localize.manifest.ts
@@ -0,0 +1,20 @@
+﻿import { Composer } from '@omnia/tooling/composers';
+import { $outputname$ } from "./localize";
+
+Composer
+    .registerManifest('$guid1$', "$outputname$")
+    .registerResources({ resourcePaths: ["./localize.js"] });
+
+Composer.registerManifest("$guid2$")
+    .registerLocalization()
+    .namespace($outputname$.namespace)
+    .add<$outputname$.locInterface>({
+        Labels: {
+            Label1: "Label 1",
+            Label2: "Label 2"
+        },
+        Features: {
+            Feature1: "Feature 1",
+            Feature2: "Feature 2"
+        }
+    });
