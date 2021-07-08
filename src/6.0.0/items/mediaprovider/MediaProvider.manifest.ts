3a2aa9664d58ea6793b7d12d5a79b9ce19d7bd29
David Jönell <david_jonell@hotmail.com>
2021-07-08 12:05:06 +0200

    Added service and media provider

---
+++ src/6.0.0/items/mediaprovider/MediaProvider.manifest.ts
@@ -0,0 +1,22 @@
+import { Composer } from '@omnia/tooling/composers';
+import { Guid } from '@omnia/fx/models';
+import { FontAwesomeIcon } from '@omnia/fx-models';
+
+
+Composer.registerManifest(new Guid("$guid1$"), "$outputname$")
+.registerWebComponent({
+    elementName: "$element$",
+    entryPoint: "./$outputname$.jsx"
+})
+.withDefinition({
+    title: "$outputname$", // You can use localization, i.e., $Localize:Namespace.Something.Title; 
+    description: "$outputname$", // You can use localization, i.e., $Localize:Namespace.Something.Description;
+    icon: new FontAwesomeIcon("far fa-smile-beam")
+})
+
+/*Registration of the provider to mediapicker*/
+Composer.registerManifest(new Guid("$guid2$"), "$outputname$.registration")
+.registerResources({
+    resourcePaths: ["./Registrations/RegistrationLoader.js"]
+})
+.extendApi(api => api.fx.ux.mediaPicker.registration);
