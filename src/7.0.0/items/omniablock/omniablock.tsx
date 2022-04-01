c0bb54ef384d7f1ab7fb65486e8dabe781c12771
Nhan Le Hoang <nhan.le.hoang@preciofishbone.se>
2022-04-01 14:19:21 +0700

    update omnia block template

--- src/7.0.0/items/omniablock/omniablock.tsx
+++ src/7.0.0/items/omniablock/omniablock.tsx
@@ -1,3 +1,4 @@
+import { Guid } from '@omnia/fx-models';
 import { StyleFlow, defineVueWebComponent, useBlockSettingsReader } from '@omnia/fx/ux';
 import { $outputname$Styles } from './$outputname$.css';
 import { $outputname$BlockSettings } from './$outputname$Settings';
@@ -7,7 +8,7 @@ export default defineVueWebComponent({
     setup(props) {
         const settings = useBlockSettingsReader<$outputname$BlockSettings>({
             defaultValue: { title: 'my block title' },
-            editElement: "$element$-settings"
+            editElementManifestId: new Guid("$guid2$")
         });
 
         const $outputname$Classes = StyleFlow.use($outputname$Styles);
